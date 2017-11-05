08384075e90703e41b393a456aa9778e2148ebf2
rapirent <rapirent@gmail.com>
2017-11-05 13:52:25 +0800

    [Fixed]removed the ack-grek, log & fixed the vscode extenstion install

--- install.sh
+++ install.sh
@@ -1,40 +1,40 @@
 #!bin/bash
 
-mkdir -p log
-
 # update
-sudo add-apt-repository ppa:jonathonf/vim -y >> ./log/update.log
-sudo add-apt-repository ppa:numix/ppa -y >> ./log/update.log
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt update -y >> ./log/update.log
-sudo apt upgrade -y >> ./log/update.log
+sudo add-apt-repository ppa:jonathonf/vim -y
+sudo add-apt-repository ppa:numix/ppa -y
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt update -y
+sudo apt upgrade -y
 
 # install basic software
-sudo apt-get install -y gcc g++ make vim curl wget zsh tmux fonts-font-awesome git tig git-flow >> ./log/install.log
+sudo apt-get install -y gcc g++ make vim curl wget zsh tmux fonts-font-awesome git tig git-flow
 
 # git
 cp ./.gitconfig $HOME
 cp ./.gitignore_global $HOME
-sudo apt-get install -y git-flow >> ./log/install.log
+sudo apt-get install -y git-flow
 
 # python
-sudo apt-get -y install python-pip >> ./log/install.log
-sudo apt-get -y install python3-pip >> ./log/install.log
-sudo pip install virtualenv -g >> ./log/install.log
+sudo apt-get -y install python-pip
+sudo apt-get -y install python3-pip
+sudo apt install virtualenv -y
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >> ./log/install.log
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp .zshrc $HOME
 chsh -s /bin/zsh
-git clone https://github.com/powerline/fonts.git /tmp
-/tmp/fonts/install.sh >> ./log/install.log
+git clone https://github.com/powerline/fonts.git /tmp/fonts
+sudo /tmp/fonts/install.sh
 gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono derivative Powerline 13'
 sudo fc-cache -v -f
+git clone https://github.com/zdharma/history-search-multi-word.git /tmp/history-search-multi-word
+sudo mkdir -p $ZSH/custom/plugins/history-search-multi-word/
+sudo cp /tmp/history-search-multi-word/history-search-multi-word.plugin.zsh $ZSH/custom/plugins/history-search-multi-word/history-search-multi-word.plugin.zsh
 
 # vim
-sudo apt-get install -y exuberant-ctags ack-grep vim-gtk >> ./log/install.log
-git clone https://github.com/rapirent/vimrc.git
-cp vimrc/_vimrc $HOME
+sudo apt-get install -y exuberant-ctags vim-gtk vim-gnome
+cp .vimrc $HOME
 vim << EOF
 :PlugUpgrade
 :PlugInstall
@@ -42,58 +42,60 @@ vim << EOF
 :quit
 EOF
 
-# sdcv 
-sudo apt-get install sdcv -y >> ./log/install.log
+# sdcv
+sudo apt-get install sdcv -y
 mkdir -p $HOME/.stardict/dic
 tar jxvf stardict-langdao-ce-big5-2.4.2.tar.bz2 -C ~/.stardict/dic
 tar jxvf stardict-langdao-ec-big5-2.4.2.tar.bz2 -C ~/.stardict/dic
 
 # theme icon
-sudo apt-get install numix-icon-theme-circle -y >> ./log/update.log
+sudo apt-get install numix-icon-theme-circle -y
 
 # chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/123.deb >> ./log/install.log
-sudo dpkg -i /tmp/123.deb >> ./log/install.log
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/123.deb
+sudo dpkg -i /tmp/123.deb
 sudo rm -r -f /tmp/123.deb
+sudo apt --fix-broken install -y
 
-# wallpaper 
+# wallpaper
 cp ./wallpaper.jpg $HOME
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/dotfiles/wallpaper.jpg"
 
 # autojump
-git clone https://github.com/joelthelion/autojump /tmp
-/tmp/autojump/make
+git clone https://github.com/joelthelion/autojump /tmp/autojump
+sudo /tmp/autojump/install.py
 
 
 # vscode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update -y  >> ./log/update.log
-sudo apt-get install code >> ./log/install.log
-list = "HookyQR.beautify abusaidm.html-snippets bibhasdn.django-html bibhasdn.django-snippets dbaeumer.vscode-eslint
-\ donjayamanne.githistory donjayamanne.python eamodio.gitlens ecmel.vscode-html-css esbenp.prettier-vscode
-\ himanoa.Python-autopep8 michelemelluso.code-beautifier ms-vscode.sublime-keybindings
-\ naumovs.color-highlight redhat.java robertohuertasm.vscode-icons vncz.vscode-apielements wholroyd.jinja
-\ xabikos.JavaScriptSnippets zhuangtongfa.Material-theme"
-for i in $list; do
-    code --install-extensions "$i"
+sudo apt update -y
+sudo apt-get install code
+list=("HookyQR.beautify" "abusaidm.html-snippets" "bibhasdn.django-html bibhasdn.django-snippets" "dbaeumer.vscode-eslint" \
+"donjayamanne.githistory" "donjayamanne.python" "eamodio.gitlens ecmel.vscode-html-css" "esbenp.prettier-vscode" \
+"himanoa.Python-autopep8" "michelemelluso.code-beautifier" "ms-vscode.sublime-keybindings" \
+"naumovs.color-highlight" "redhat.java robertohuertasm.vscode-icons" "vncz.vscode-apielements wholroyd.jinja" \
+"xabikos.JavaScriptSnippets" "zhuangtongfa.Material-theme")
+for i in "${list[@]}"; do
+    code --install-extension "$i"
 done
-cp keybindings.json $HOME/.config/Code/User
-cp settings.json $home/.config/Code/User
+sudo mkdir -p ~/.config/Code/User
+cp keybindings.json ~/.config/Code/User
+cp settings.json ~/.config/Code/User
 
 
 # S.M.A.R.T
-sudo apt-get -y install smartmontools >> ./log/install.log
+sudo apt-get -y install smartmontools
 
 # java
-sudo apt-get -y oracle-java8-installer >> ./log/install.log
+sudo apt-get -y install oracle-java8-installer
 
 # spotify
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get -y update >> ./log/update.log
-sudo apt-get -y install spotify-client >> ./log/install.log
+sudo apt-get -y update
+sudo apt-get -y install spotify-client
 
 sudo apt -y autoremove
