ca48e3d2d347305f56e073a6420da94290829d29
kuoteng <kuoteng.ding@gmail.com>
2020-05-06 22:50:03 +0800

    re-arrange

--- macOS/install.sh
+++ macOS/install.sh
@@ -9,14 +9,14 @@ cp .zshrc $HOME
 chsh -s /bin/zsh
 git clone https://github.com/powerline/fonts.git
 sudo ~/install.sh
-gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono derivative Powerline 13'
-sudo fc-cache -v -f
+# now you should configure the fonts and theme in your terminal program
 git clone https://github.com/zdharma/history-search-multi-word.git /tmp/history-search-multi-word
 sudo cp -r /tmp/history-search-multi-word $ZSH/custom/plugins/history-search-multi-word
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # vim
-brew install vim
+# brew install vim
+# vim is pre-installed in macOS
 cp .vimrc $HOME
 vim << EOF
 :PlugUpgrade
